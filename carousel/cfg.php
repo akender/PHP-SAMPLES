58bfa8589c3f55af665c5fc824af8f48535c904d
akender <arno.kender@gmail.com>
2017-08-17 21:30:36 +0300

    Create cfg.php

---
+++ carousel/cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+//:::DEBUGGING:::
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//:::DIR:::
+$cfg['dir']['base']     = './';
+$cfg['dir']['img']      = 'images/';
+
+?>
