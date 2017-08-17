eecaf43e28736620dd323246d7c8fb7bf9a643d0
akender <arno.kender@gmail.com>
2017-08-17 21:40:09 +0300

    Update index.php

--- carousel/index.php
+++ carousel/index.php
@@ -2,7 +2,7 @@
 
 //:::REQUIRE:::
 require_once 'cfg.php';
-require_once 'lib/functions.inc.php';
+require_once '../lib/functions.inc.php';
 
 $files = get_files($cfg['dir']['base'].$cfg['dir']['img']);
 
@@ -15,9 +15,9 @@ $files = get_files($cfg['dir']['base'].$cfg['dir']['img']);
     <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </head>
 
 <body>
