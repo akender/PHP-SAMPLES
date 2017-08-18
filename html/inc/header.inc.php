0d2aeb618b3f0ccab82a89d9ec93c61afca0afc6
akender <arno.kender@gmail.com>
2017-08-18 23:28:30 +0300

    Create header.inc.php

---
+++ html/inc/header.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="'.$cfg['dir']['base'].'css/bootstrap.min.css">
+    <!-- JavaScript -->
+    <script src="'.$cfg['dir']['base'].'js/jquery.min.js"></script>
+    <script src="'.$cfg['dir']['base'].'js/bootstrap.min.js"></script>
+';
+
+?>
