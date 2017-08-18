fc78563f81865ea4aaa17575a98f3c96884499f4
akender <arno.kender@gmail.com>
2017-08-18 23:24:49 +0300

    Update and rename carousel/cfg.php to html/carousel/cfg.php

---
+++ html/carousel/cfg.php
@@ -0,0 +1,11 @@
+<?php
+
+// ::: DIR :::
+$cfg['dir']['base']     = '../../';
+
+// ::: REQUIRE :::
+require_once $cfg['dir']['base'].'cfg-default.php';
+require_once $cfg['dir']['base'].'cfg-global.php';
+require_once $cfg['dir']['base'].'lib/functions.inc.php';
+
+?>
